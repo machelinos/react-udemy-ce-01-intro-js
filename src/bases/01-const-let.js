c5e60b32be9ce5807800c9f95f6d611e71bc728d
machelinos <marcel.cabrera@gmail.com>
2023-05-24 01:52:51 -0500

    01 const let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+const firstName = 'Marcel'
+const lastName = 'Cabrera'
+
+console.log(`${firstName} ${lastName}`)
+
+let givenValue = 10
+console.log(givenValue)
+
+if (true) {
+  let givenValue = 5
+  console.log(givenValue)
+}
+
+console.log(givenValue)
