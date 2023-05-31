0546d0d6f1c9b9b21a13ad60bec96d248801f281
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-05-31 02:54:16 -0600

    11 async await

---
+++ src/bases/11-async-await.js
@@ -0,0 +1,29 @@
+const getImagen = async () => {
+  const apiKey = 'K2bFwNaKbTtfO37edtMPd4E4uwNmOBvK'
+  try {
+    const fetchResp = await fetch(
+      `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`,
+    )
+
+    const { data } = await fetchResp.json()
+
+    return data.images.original.url
+  } catch (error) {
+    return null
+  }
+}
+
+const showImg = async () => {
+  try {
+    const imgUrl = await getImagen()
+    if (imgUrl) {
+      const img = document.createElement('img')
+      img.src = imgUrl
+      document.body.append(img)
+    }
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+showImg()
